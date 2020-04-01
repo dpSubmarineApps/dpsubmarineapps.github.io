f1f1d747745f7866a8412417ccd75a30f523eaba
Adan Romero <adan_romero@hcsc.net>
2020-04-01 15:37:51 -0500

    Updates

---
+++ precache-manifest.73cfe4faae22478e5866c854712109b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b034e7ff29bc5308d69c1c5da0293511",
+    "url": "/flippin-game/index.html"
+  },
+  {
+    "revision": "dd4b4a74314b71685c08",
+    "url": "/flippin-game/static/css/main.a66ba469.chunk.css"
+  },
+  {
+    "revision": "234a0cf653d7f144594b",
+    "url": "/flippin-game/static/js/2.4ea74634.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/flippin-game/static/js/2.4ea74634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4b4a74314b71685c08",
+    "url": "/flippin-game/static/js/main.eccdc2f6.chunk.js"
+  },
+  {
+    "revision": "65ba436f0125766125b3",
+    "url": "/flippin-game/static/js/runtime-main.4df2bbca.js"
+  },
+  {
+    "revision": "ef948819abf37b8a10018a2b3519e965",
+    "url": "/flippin-game/static/media/DigitalLogo.ef948819.png"
+  },
+  {
+    "revision": "e514ebeecb72665b5f6b603e19ce0f3b",
+    "url": "/flippin-game/static/media/simpsons-house.e514ebee.jpg"
+  },
+  {
+    "revision": "07af4b626eeae4abe9297340847df92a",
+    "url": "/flippin-game/static/media/simpsonsThemeSong.07af4b62.mp3"
+  }
+]);
